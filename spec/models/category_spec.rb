2df9b7c98b10f65b9ce9b06e7291f20f2f03a0c1
bielFernandes <gabrielfdias082@gmail.com>
2023-11-12 12:38:14 -0300

    add comentarios explicando validações

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,5 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   it { is_expected.to validate_presence_of(:name) }
+  #COMENTÁRIO:: valida o campo name do model Category
+  #ps: esses métodos são do shoulda matchers, facilitam a sintaxe
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+  #COMENTÁRIO:: valida que não pode ter o mesmo nome 
 end
