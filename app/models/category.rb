908899d7bb85d78f169cc3ad81ba5054ab3801ef
bielFernandes <gabrielfdias082@gmail.com>
2023-11-12 13:05:44 -0300

    add coment.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
     validates :name, presence: true, uniqueness:{ case_sensitive: false }
+    #define que o campo name é único(nao vai se repetir) e case_sensitive
     #validação criada em spec/models/category_spec.rb
 end
