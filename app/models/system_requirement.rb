657cf4541b5f26b0c72cea858503f9b18504b47a
bielFernandes <gabrielfdias082@gmail.com>
2023-11-12 13:05:37 -0300

    adicionando validações criadas em rspec/models/...

--- app/models/system_requirement.rb
+++ app/models/system_requirement.rb
@@ -1,2 +1,11 @@
 class SystemRequirement < ApplicationRecord
+
+    #validação criada em spec/models/system_requirement.rb
+
+    validates :name, presence: true, uniqueness:{ case_sensitive: false }
+    #define que o campo name é único(nao vai se repetir) e case_sensitive
+    validates :operational_system, presence: true
+    validates :processor, presence: true
+    validates :memory, presence: true
+    validates :video_board, presence: true
 end
