f2763065143747d6e4e0d2b709972f43ef6ac762
bielFernandes <gabrielfdias082@gmail.com>
2023-11-12 03:31:17 -0300

    email troca d senha rails g devise:views -v mailer

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -8,6 +8,8 @@ DeviseTokenAuth.setup do |config|
   config.change_headers_on_each_request = true
   #COMENTÁRIO = FAZ O HEADER ATUALIZAR A CADA TOKEN
 
+  config.require_client_password_reset_token = true
+
   # By default, users will need to re-authenticate after 2 weeks. This setting
   # determines how long tokens will remain valid after they are issued.
   config.token_lifespan = 1.weeks
